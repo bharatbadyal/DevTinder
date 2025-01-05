c093c198c771b27c1730bce6e562fc8420a54c49
Bharat <myprof219@gmail.com>
2025-01-05 22:00:38 +0530

    Objects

--- BasicJS/Boxing.js
+++
@@ -1,9 +0,0 @@
-// Boxing or Auto Boxing : Primitive types trying to get converted to non primitive automatically
-// x = 1;
-// console.log(x.toString());
-
-const fs = require("fs");
-
-fs.readFile("./random.txt", (data) => {
-  console.log(data);
-});
